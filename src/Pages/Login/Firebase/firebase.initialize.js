d968f194d8602e76499e77c360754b55802da5a2
BGI-HO-IT-Exe-Sharif <it22@bengal.com.bd>
2023-08-21 17:11:51 +0600

    firebase connection and file structures

---
+++ src/Pages/Login/Firebase/firebase.initialize.js
@@ -0,0 +1,9 @@
+// console.log(process.env);
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./firebase.config";
+ 
+const initializeAuthentication = () =>{ 
+    initializeApp(firebaseConfig);
+}
+ 
+export default initializeAuthentication;
